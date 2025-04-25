2bab02ea657076af2569fe8530147496d5562f5f
leqaadeeb <leqaadeeb8@gmail.com>
2025-04-25 12:12:09 -0700

    update TreeNode class

--- src/tree/FamilyTree.java
+++ src/tree/FamilyTree.java
@@ -24,18 +24,24 @@ public class FamilyTree {
         void addChild(TreeNode childNode) {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+            children.add(childNode);
+            childNode.parent = this;
         }
 
         // Searches subtree at this node for a node
         // with the given name. Returns the node, or null if not found.
         TreeNode getNodeWithName(String targetName) {
             // Does this node have the target name?
-            if (?????)
+            if (name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
+                if(child.getNodeWithName(targetName) != null)
+                {
+                    return child.getNodeWithName(targetName);
+                }
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
             }
@@ -50,12 +56,19 @@ public class FamilyTree {
         ArrayList<TreeNode> collectAncestorsToList() {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
 
+            TreeNode nde = this.parent;
+
+            while(current != null)
+            {
+                ancestors.add(current);
+                current = current.parent;
+
+            }
+            return ancestors;
             // ????? Collect ancestors of this TreeNode into the array list. HINT: going up
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
-
-            return ancestors;
         }
 
         public String toString() {
